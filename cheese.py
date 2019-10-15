b1b24d8b323cf08d00f87f7521a89049fe567baf
Koki Muraoka <koki.muraoka@gmail.com>
2019-10-14 19:14:05 -0700

    Proceed

--- cheese.py
+++ cheese.py
@@ -1,8 +1,74 @@
 import cv2
 import os
+import numpy as np
+import math
 
 cap = cv2.VideoCapture(0)
-ret, frame = cap.read()
+ret, img = cap.read()
 
-cv2.imwrite('captured.jpg', frame)
-os.system("gpup captured.jpg")
+
+# frame = cv2.equalizeHist(frame)
+
+
+
+
+def apply_mask(matrix, mask, fill_value):
+    masked = np.ma.array(matrix, mask=mask, fill_value=fill_value)
+    return masked.filled()
+
+def apply_threshold(matrix, low_value, high_value):
+    low_mask = matrix < low_value
+    matrix = apply_mask(matrix, low_mask, low_value)
+
+    high_mask = matrix > high_value
+    matrix = apply_mask(matrix, high_mask, high_value)
+
+    return matrix
+
+def simplest_cb(img, percent):
+    assert img.shape[2] == 3
+    assert percent > 0 and percent < 100
+
+    half_percent = percent / 200.0
+
+    channels = cv2.split(img)
+
+    out_channels = []
+    for channel in channels:
+        assert len(channel.shape) == 2
+        # find the low and high precentile values (based on the input percentile)
+        height, width = channel.shape
+        vec_size = width * height
+        flat = channel.reshape(vec_size)
+
+        assert len(flat.shape) == 1
+
+        flat = np.sort(flat)
+
+        n_cols = flat.shape[0]
+
+        low_val  = flat[math.floor(n_cols * half_percent)]
+        high_val = flat[math.ceil( n_cols * (1.0 - half_percent))]
+
+        print("Lowval: {}".format(low_val))
+        print("Highval: {}".format(high_val))
+
+        # saturate below the low percentile and above the high percentile
+        thresholded = apply_threshold(channel, low_val, high_val)
+        # scale the channel
+        normalized = cv2.normalize(thresholded, thresholded.copy(), 0, 255, cv2.NORM_MINMAX)
+        out_channels.append(normalized)
+
+    return cv2.merge(out_channels)
+
+img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+
+img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
+
+img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
+
+# img_output = simplest_cb(img_output, 20)
+
+cv2.imwrite('/tmp/captured1.jpg', img_output)
+os.system("/usr/local/bin/gpup /tmp/captured1.jpg")
+
